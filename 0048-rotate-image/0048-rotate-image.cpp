c6a2203921b6caf3c5821dc91c651bbcdacefe2e
arya2002shukla <85741754+arya2002shukla@users.noreply.github.com>
2023-06-16 11:12:58 +0530

    Time: 0 ms (100%), Space: 7.1 MB (19.04%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,27 +1,46 @@
+// class Solution {
+// public:
+//     void rotate(vector<vector<int>>& matrix) {
+//         int s = matrix.size();
+        
+        
+//         vector<vector<int>> rotate;
+//         for(int j=0;j<s;j++){
+//             vector<int>v1;
+//             for(int i=s-1;i>=0;i--){
+//                 v1.push_back(matrix[i][j]);
+//             }
+//             rotate.push_back(v1);
+//         }
+
+//         matrix.clear();
+
+//         for(int i=0;i<s;i++){
+//             vector<int>v2;
+//             for(int j=0;j<s;j++){
+//                 v2.push_back(rotate[i][j]);
+//             }
+//             matrix.push_back(v2);
+//         }
+        
+//     }
+// };
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int s = matrix.size();
-        
-        
-        vector<vector<int>> rotate;
-        for(int j=0;j<s;j++){
-            vector<int>v1;
-            for(int i=s-1;i>=0;i--){
-                v1.push_back(matrix[i][j]);
-            }
-            rotate.push_back(v1);
+        int n=matrix.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++)
+            swap(matrix[i][j],matrix[j][i]);
         }
-
-        matrix.clear();
-
-        for(int i=0;i<s;i++){
-            vector<int>v2;
-            for(int j=0;j<s;j++){
-                v2.push_back(rotate[i][j]);
-            }
-            matrix.push_back(v2);
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
         }
-        
     }
 };
+
+
+
+
+
+
