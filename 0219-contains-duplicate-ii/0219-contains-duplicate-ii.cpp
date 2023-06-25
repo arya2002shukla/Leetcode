b46522b1dfc1e9a727bbe2becba19c5d962ae2ea
arya2002shukla <85741754+arya2002shukla@users.noreply.github.com>
2023-06-25 22:48:13 +0530

    Time: 198 ms (58.84%), Space: 77.4 MB (28.86%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
-    // bool containsNearbyDuplicate(vector<int>& nums, int k) {
-    //     map<int,int>m1;
-    //     int n =nums.size();
-    //     for(int i=0;i<n;i++){
-    //         m1[nums[i]]=i;
-    //     }
-        
-    //     for(int i=0;i<n;i++){
-    //         if(m1[nums[i]]!=i){
-    //             if(abs(i-m1[nums[i]])<=k){
-    //                 return true;
-    //             }
-    //         }
-    //     }
-    //     return false;
-    // }
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-    map<int, int> m1;
-    int n = nums.size();
-    for (int i = 0; i < n; i++) {
-        if (m1.find(nums[i]) != m1.end()) {
-            if (abs(i - m1[nums[i]]) <= k) {
-                return true;
+        unordered_map<int,int>m1;
+        int n =nums.size();
+
+        
+        for(int i=0;i<n;i++){
+            if(m1.find(nums[i])!=m1.end() && abs(i-m1[nums[i]])<=k){
+                // if(abs(i-m1[nums[i]])<=k){
+                    return true;
+                // }
             }
+            m1[nums[i]]=i;
         }
-        m1[nums[i]] = i;
+        return false;
     }
-    return false;
-}
+  
 
 };
