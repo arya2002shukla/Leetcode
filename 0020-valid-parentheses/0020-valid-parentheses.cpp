19ba3799cee9f612cf5824775ae759b329505ee6
arya2002shukla <85741754+arya2002shukla@users.noreply.github.com>
2023-06-27 18:39:04 +0530

    Time: 4 ms (40.35%), Space: 6.3 MB (78.97%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.length();
+        stack<char>stk;
+        if(n==1){
+            return false;
+        }
+        
+        for(int i=0;i<n;i++){
+            switch (s[i]){
+            case '{':
+                stk.push(s[i]);
+                break;
+            case '[':
+                stk.push(s[i]);
+                break;
+            case '(':
+                stk.push(s[i]);
+                break;
+            case '}':
+                if(stk.empty() || stk.top()!='{'){
+                    return false;
+                }
+                else{
+                    stk.pop();
+                }
+                
+                break;
+            case ']':
+                if(stk.empty() || stk.top()!='['){
+                    return false;
+                }
+                else{
+                    stk.pop();
+                }
+                break;
+            case ')':
+                if(stk.empty() || stk.top()!='('){
+                    return false;
+                }
+                else{
+                    stk.pop();
+                }
+                break;
+            }
+        }
+
+        if(stk.empty()){
+            return true;
+        }
+         return false;
+    }
+};
