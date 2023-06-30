bb23e02199bad99adfe504a9ac41bccf49dcd938
arya2002shukla <85741754+arya2002shukla@users.noreply.github.com>
2023-06-30 23:15:03 +0530

    Time: 8 ms (50.54%), Space: 9.1 MB (15.75%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,36 +1,3 @@
-// class Solution {
-// public:
-//     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-//         int n=nums2.size();
-//         int k=nums1.size();
-//         map<int,int>map1;
-//         for(int i=0;i<n;i++){
-//             map1[nums2[i]]=i;
-//         }
-//         vector<int>pos;
-//         vector<int>output;
-//         for(int i=0;i<k;i++){
-//             if(map1[nums1[i]]){
-//                 pos.push_back(map1[nums1[i]]);
-//             }
-            
-//         }
-
-//         for(int i=0;i<n;i++){
-//             if(nums2[pos[i]+1]<nums2[pos[i]]){
-//                 output.push_back(-1);
-//             }
-//             else{
-//                 output.push_back(nums2[pos[i]+1]);
-               
-//             }
-//         }
-//         return output;
-//     }
-// };
-#include <vector>
-#include <map>
-
 class Solution {
 public:
     std::vector<int> nextGreaterElement(std::vector<int>& nums1, std::vector<int>& nums2) {
@@ -40,8 +7,8 @@ public:
         for (int i = 0; i < n; i++) {
             map1[nums2[i]] = i;
         }
-        std::vector<int> pos;
-        std::vector<int> output;
+        vector<int> pos;
+        vector<int> output;
         for (int i = 0; i < k; i++) {
             if (map1.find(nums1[i]) != map1.end()) {
                 pos.push_back(map1[nums1[i]]);
