d0b40d1a3e775e057f6ebc43af8fa4e59c488882
arya2002shukla <85741754+arya2002shukla@users.noreply.github.com>
2023-06-30 23:52:24 +0530

    Time: 6 ms (97.16%), Space: 8 MB (71.08%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -17,6 +17,7 @@ public:
         ListNode* fast = head;
 
         while(fast && fast->next){
+            
             slow=slow->next;
             fast=fast->next->next;
             if(slow==fast){
